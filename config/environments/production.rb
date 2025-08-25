877d305a2d4ddea35bd8f744aaaf6a161dddc4ea
Kosei-Higashiya <koseish1107@gmail.com>
2025-08-25 20:49:35 +0900

    file修正６

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.hosts << "original-app-loopin.onrender.com"
-  config.hosts = nil
 
 
   # Enable DNS rebinding protection and other `Host` header attacks.
