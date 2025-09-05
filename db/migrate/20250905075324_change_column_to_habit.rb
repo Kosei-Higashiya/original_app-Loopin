929935b5a8a39c752dd27b8b991ee29b2e52f1b3
Kosei-Higashiya <koseish1107@gmail.com>
2025-09-05 17:03:31 +0900

    詳細、アクティブ機能削除

---
+++ db/migrate/20250905075324_change_column_to_habit.rb
@@ -0,0 +1,6 @@
+class ChangeColumnToHabit < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :habits, :active, :boolean
+    remove_index :habits, name: "index_habits_on_active" if index_exists?(:habits, :active)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_01_151215) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_05_075324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,10 +34,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_01_151215) do
     t.string "title", null: false
     t.text "description"
     t.bigint "user_id", null: false
-    t.boolean "active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["active"], name: "index_habits_on_active"
     t.index ["user_id"], name: "index_habits_on_user_id"
   end
 
