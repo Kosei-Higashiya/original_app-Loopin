fe7cac8455272e6f1e17ddca0ebd2c70d1faef25
Kosei-Higashiya <koseish1107@gmail.com>
2025-09-06 04:16:42 +0900

    投稿機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_05_075324) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_05_141505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_05_075324) do
     t.index ["user_id"], name: "index_habits_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "habit_id", null: false
+    t.text "content"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_posts_on_created_at"
+    t.index ["habit_id"], name: "index_posts_on_habit_id"
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -54,4 +67,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_05_075324) do
   add_foreign_key "habit_records", "habits"
   add_foreign_key "habit_records", "users"
   add_foreign_key "habits", "users"
+  add_foreign_key "posts", "habits"
+  add_foreign_key "posts", "users"
 end
